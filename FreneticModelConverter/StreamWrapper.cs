f91a608f560893eea53b551ab0cd6f6c253b7fbd
Alex 'mcmonkey' Goodwin <agoodwin@freneticllc.com>
2020-04-09 03:53:03 -0700

    add license headers to code files

--- FreneticModelConverter/StreamWrapper.cs
+++ FreneticModelConverter/StreamWrapper.cs
@@ -1,3 +1,11 @@
+//
+// This file is part of the Frenetic Converter Engine, created by Frenetic LLC.
+// This code is Copyright (C) Frenetic LLC under the terms of a strict license.
+// See README.md or LICENSE.txt in the FreneticModelConverter source root for the contents of the license.
+// If neither of these are available, assume that neither you nor anyone other than the copyright holder
+// hold any right or permission to use this software until such time as the official license is identified.
+//
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
